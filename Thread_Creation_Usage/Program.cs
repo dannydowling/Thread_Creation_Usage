f7d9ca665ba952292c5e07baa6aa9ab9eb3a1988
Danny Dowling <danny.dowling@gmail.com>
2022-02-20 14:14:43 -0900

    Add project files.

---
+++ Thread_Creation_Usage/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+ThreadOverhead();
+
+static void ThreadOverhead()
+{
+    const Int32 OneMB = 1024 * 1024;
+    using (ManualResetEvent wakeThreads = new ManualResetEvent(false))
+    {
+        string pause = "";
+
+        while (true)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i <= 1000; i++)
+                {
+                    
+                    if (pause != String.Empty)
+                    {
+                        wakeThreads.Set();
+                        Debugger.Break();
+                    }
+                    Console.WriteLine("current thread is thread number {0}", i);
+
+                    Console.WriteLine("{0}: {1}MB Nonpaged", ++i, Process.GetCurrentProcess().NonpagedSystemMemorySize64 / OneMB);
+                    Console.WriteLine("{0}: {1} Handles", ++i, Process.GetCurrentProcess().HandleCount );
+                    Console.WriteLine("{0}: {1}MB Paged", ++i, Process.GetCurrentProcess().PagedMemorySize64);
+                    Console.WriteLine("___________________________________");
+
+                    pause = Console.ReadLine();
+                }
+            });
+        }        
+    };
+}
